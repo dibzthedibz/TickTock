5df4ec693f0c709c1bc90ffb2709d02f51877bcb
Ryan <Dibzthedibz@gmail.com>
2021-06-01 12:32:11 -0700

    Initial Comment Class Commit

---
+++ 24Hour.Data/Migrations/202106011928371_CommentCreated.cs
@@ -0,0 +1,16 @@
+namespace _24Hour.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CommentCreated : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
